a90ae6ec988718702c038abc3d805c25f24429e8
Hemanth Adepu <rqvxbpddz2@privaterelay.appleid.com>
2022-09-20 12:05:14 +0000

    Update App.js

--- Tesla08/src/App.js
+++ Tesla08/src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        Tesla
         <Header />
         <TeslaBattery counterDefaultVal={counterDefaultVal}/>
       </div>
